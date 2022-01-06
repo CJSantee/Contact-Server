f2ca306313df58fea6e8800725b4115e2e36423e
CJSantee <santeedev@gmail.com>
2022-01-05 21:44:06 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+
+
+router.get("/", (req, res) => {
+    res.json({message: "IT WORDS"});
+});
